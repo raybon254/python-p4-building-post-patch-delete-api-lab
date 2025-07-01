5acdc29b7e5fc903bda360d886718801dbc5e916
Brian <musyimib449@gmail.com>
2025-07-01 03:10:43 +0300

    CRUDS lab

--- server/app.py
+++ server/app.py
@@ -14,22 +14,67 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+
 @app.route('/')
 def home():
     return '<h1>Bakery GET-POST-PATCH-DELETE API</h1>'
 
+
 @app.route('/bakeries')
 def bakeries():
     bakeries = [bakery.to_dict() for bakery in Bakery.query.all()]
-    return make_response(  bakeries,   200  )
+    return make_response(bakeries, 200)
 
-@app.route('/bakeries/<int:id>')
+@app.route('/bakeries/<int:id>', methods=['GET', 'PATCH'])
 def bakery_by_id(id):
-
     bakery = Bakery.query.filter_by(id=id).first()
-    bakery_serialized = bakery.to_dict()
-    return make_response ( bakery_serialized, 200  )
+    if request.method == 'GET':
+        bakery_serialized = bakery.to_dict()
+        return make_response ( bakery_serialized, 200  )
+    elif request.method == 'PATCH':
+        for attr in request.form:
+            setattr(bakery, attr, request.form.get(attr))
 
+        db.session.add(bakery)
+        db.session.commit()
+
+        bakery_dict = bakery.to_dict()
+
+        response = make_response(
+            bakery_dict,
+            200
+        )
+
+        return response
+
+@app.route('/baked_goods', methods=['POST'])
+def create_baked_good():
+    data = request.form or request.get_json()  # Handles both form and JSON
+    new_baked_good = BakedGood(
+        name=data.get("name"),
+        price=data.get("price")
+    )
+    db.session.add(new_baked_good)
+    db.session.commit()
+
+    return make_response(new_baked_good.to_dict(), 201)
+
+
+@app.route('/baked_goods/<int:id>', methods=['DELETE'])
+def delete_baked_good(id):
+    baked_good = BakedGood.query.get(id)
+
+    if not baked_good:
+        return make_response({"message": "Record Does Not Exist"}, 404)
+
+    db.session.delete(baked_good)
+    db.session.commit()
+
+    return make_response({
+        "delete_successful": True,
+        "message": "Baked good deleted."
+    }, 200)
+ 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
     baked_goods_by_price = BakedGood.query.order_by(BakedGood.price.desc()).all()
